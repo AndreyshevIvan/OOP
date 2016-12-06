e5853130e97ac5d30d9d08d26954d181f432ee8f
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2016-12-06 14:48:09 +0300

    Создан проект Лаб.3

---
+++ Lab3/CCar/CCar/main.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "CCar.h"
+
+int main()
+{
+
+	return 0;
+}
---
+++ Lab3/CCar/CCarTest/CCarTest.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "..\CCar\CCar.h"
+#include <vector>
+
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(tested_functions)
+
+BOOST_AUTO_TEST_CASE(test_name)
+{
+	BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
