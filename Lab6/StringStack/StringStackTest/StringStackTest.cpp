948274deffccf7c1c2df308deb59139c39962a8e
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-01-15 22:36:57 +0300

    Добавлен прокт StringStack

---
+++ Lab6/StringStack/StringStackTest/StringStackTest.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "../StringStack/StringStack.h"
+
+BOOST_AUTO_TEST_SUITE(test_suite)
+
+	BOOST_AUTO_TEST_CASE(case_one)
+	{
+		BOOST_CHECK(true);
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
