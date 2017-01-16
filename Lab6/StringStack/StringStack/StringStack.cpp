7efb5c034a6879061dd090762d02f83807a72a87
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-01-16 23:31:33 +0300

    Методы вынесены в отдельный файл

--- Lab6/StringStack/StringStack/StringStack.cpp
+++ Lab6/StringStack/StringStack/StringStack.cpp
@@ -1,2 +1,103 @@
 #include "stdafx.h"
 #include "StringStack.h"
+
+StringStack::StringStack(StringStack const& stack)
+{
+	CopyNodes(stack);
+}
+
+StringStack::StringStack(StringStack && cloneStack)
+{
+	m_top = cloneStack.m_top;
+	m_stackSize = cloneStack.m_stackSize;
+	cloneStack.m_top = nullptr;
+	cloneStack.m_stackSize = 0;
+}
+
+void StringStack::Push(std::string const& element)
+{
+	auto newNode = std::make_shared<Node>(element, m_top);
+	m_top = newNode;
+	m_stackSize++;
+}
+
+std::string StringStack::GetTop() const
+{
+	if (Empty())
+	{
+		throw std::logic_error("Imposible return content from empty stack");
+	}
+
+	return m_top->content;
+}
+
+void StringStack::Pop()
+{
+	if (!Empty())
+	{
+		m_top->content.~basic_string();
+		m_top = m_top->next;
+		m_stackSize--;
+	}
+}
+
+StringStack& StringStack::operator=(StringStack const& cloneStack)
+{
+	if (this != &cloneStack)
+	{
+		Clear();
+		CopyNodes(cloneStack);
+	}
+
+	return *this;
+}
+
+StringStack& StringStack::operator=(StringStack && moveStack)
+{
+	if (this != &moveStack)
+	{
+		m_stackSize = moveStack.m_stackSize;
+		m_top = moveStack.m_top;
+		moveStack.m_top = nullptr;
+		moveStack.m_stackSize = 0;
+	}
+
+	return *this;
+}
+
+bool StringStack::Empty() const
+{
+	return (m_stackSize == 0);
+}
+
+void StringStack::Clear()
+{
+	while (!Empty())
+	{
+		m_top->content.~basic_string();
+		Pop();
+	}
+}
+
+void StringStack::CopyNodes(StringStack const& stack)
+{
+	if (!stack.Empty())
+	{
+		std::shared_ptr<Node> pCopiedNode = stack.m_top;
+
+		m_top = std::make_shared<Node>(*pCopiedNode);
+		auto pPasteNode = m_top;
+
+		pPasteNode->content = pCopiedNode->content;
+
+		while (pCopiedNode->next != nullptr)
+		{
+			pPasteNode->next = std::make_shared<Node>(*pCopiedNode->next);
+
+			pCopiedNode = pCopiedNode->next;
+			pPasteNode = pPasteNode->next;
+		}
+
+		m_stackSize = stack.m_stackSize;
+	}
+}
--- Lab6/StringStack/StringStack/StringStack.h
+++ Lab6/StringStack/StringStack/StringStack.h
@@ -1,90 +1,26 @@
 #pragma once
 #include "stdafx.h"
 
-class CStack
+class StringStack
 {
 public:
-	CStack() = default;
+	StringStack() = default;
 
-	CStack(CStack const& stack)
-	{
-		CopyNodes(stack);
-	}
+	StringStack(StringStack const& stack);
+	StringStack(StringStack && cloneStack);
 
-	CStack(CStack && cloneStack)
-	{
-		m_top = cloneStack.m_top;
-		m_stackSize = cloneStack.m_stackSize;
-		cloneStack.m_top = nullptr;
-		cloneStack.m_stackSize = 0;
-	}
+	void Push(std::string const& element);
+	void Pop();
+	std::string GetTop() const;
 
-	void Push(std::string const& element)
-	{
-		auto newNode = std::make_shared<Node>(element, m_top);
-		m_top = newNode;
-		m_stackSize++;
-	}
+	StringStack& operator=(StringStack const& cloneStack);
+	StringStack& operator=(StringStack && moveStack);
 
-	std::string GetTop() const
-	{
-		if (Empty())
-		{
-			throw std::logic_error("Imposible return content from empty stack");
-		}
+	bool Empty() const;
 
-		return m_top->content;
-	}
+	void Clear();
 
-	void Pop()
-	{
-		if (!Empty())
-		{
-			m_top->content.~basic_string();
-			m_top = m_top->next;
-			m_stackSize--;
-		}
-	}
-
-	CStack& operator=(CStack const& cloneStack)
-	{
-		if (this != &cloneStack)
-		{
-			Clear();
-			CopyNodes(cloneStack);
-		}
-
-		return *this;
-	}
-
-	CStack& operator=(CStack && moveStack)
-	{
-		if (this != &moveStack)
-		{
-			m_stackSize = moveStack.m_stackSize;
-			m_top = moveStack.m_top;
-			moveStack.m_top = nullptr;
-			moveStack.m_stackSize = 0;
-		}
-
-		return *this;
-	}
-
-	bool Empty() const
-	{
-		return (m_stackSize == 0);
-	}
-
-	void Clear()
-	{
-		while (!Empty())
-		{
-			m_top->content.~basic_string();
-			Pop();
-		}
-	}
-
-	~CStack()
+	~StringStack()
 	{
 		Clear();
 	}
@@ -101,28 +37,7 @@ private:
 		std::shared_ptr<Node> next = nullptr;
 	};
 
-	void CopyNodes(CStack const& stack)
-	{
-		if (!stack.Empty())
-		{
-			std::shared_ptr<Node> pCopiedNode = stack.m_top;
-
-			m_top = std::make_shared<Node>(*pCopiedNode);
-			auto pPasteNode = m_top;
-
-			pPasteNode->content = pCopiedNode->content;
-
-			while (pCopiedNode->next != nullptr)
-			{
-				pPasteNode->next = std::make_shared<Node>(*pCopiedNode->next);
-
-				pCopiedNode = pCopiedNode->next;
-				pPasteNode = pPasteNode->next;
-			}
-
-			m_stackSize = stack.m_stackSize;
-		}
-	}
+	void CopyNodes(StringStack const& stack);
 
 	size_t m_stackSize = 0;
 	std::shared_ptr<Node> m_top = nullptr;
