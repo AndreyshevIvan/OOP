3fed5a62b0fbd07fb16ebcbd542be9493a2ba1aa
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-01-17 23:27:31 +0300

    Исправлены ошибки в CStack

--- Lab7/CMyStack/CMyStackTest/CMyStackTest.cpp
+++ Lab7/CMyStack/CMyStackTest/CMyStackTest.cpp
@@ -8,7 +8,7 @@ CStack<float> floatStack;
 
 BOOST_AUTO_TEST_SUITE(Stack_can)
 
-	BOOST_AUTO_TEST_SUITE(push_and_return)
+	BOOST_AUTO_TEST_SUITE(push_and_return_elements)
 
 		BOOST_AUTO_TEST_CASE(string)
 		{
@@ -33,15 +33,43 @@ BOOST_AUTO_TEST_SUITE(Stack_can)
 
 	BOOST_AUTO_TEST_SUITE_END()
 
+	BOOST_AUTO_TEST_SUITE(pop_elements_when)
+
+		BOOST_AUTO_TEST_CASE(stack_is_not_empty)
+		{
+			CStack<int> numbersStack;
+			int start = 0;
+			int end = 10;
+
+			for (int number = start; number <= end; number++)
+			{
+				numbersStack.Push(number);
+			}
+
+			for (int reverseNumber = end - 1; reverseNumber >= start; reverseNumber--)
+			{
+				numbersStack.Pop();
+				BOOST_CHECK_EQUAL(reverseNumber, numbersStack.GetTop());
+			}
+		}
+
+		BOOST_AUTO_TEST_CASE(stack_is_empty)
+		{
+			CStack<float> floatStack;
+			BOOST_REQUIRE_NO_THROW(floatStack.Pop());
+		}
+
+	BOOST_AUTO_TEST_SUITE_END()
+
 	BOOST_AUTO_TEST_SUITE(return_information_about_empty_when)
 
-		BOOST_AUTO_TEST_CASE(stack_empty)
+		BOOST_AUTO_TEST_CASE(stack_is_empty)
 		{
 			CStack<std::string> stack;
 			BOOST_CHECK(stack.Empty());
 		}
 
-		BOOST_AUTO_TEST_CASE(stack_not_empty)
+		BOOST_AUTO_TEST_CASE(stack_is_not_empty)
 		{
 			CStack<int> stack;
 			stack.Push(1000);
@@ -72,14 +100,14 @@ BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(Clear_stack_when)
 
-	BOOST_AUTO_TEST_CASE(stack_empty)
+	BOOST_AUTO_TEST_CASE(stack_is_empty)
 	{
 		CStack<std::string> stack;
 		stack.Clear();
 		BOOST_CHECK(stack.Empty());
 	}
 
-	BOOST_AUTO_TEST_CASE(stack_not_empty)
+	BOOST_AUTO_TEST_CASE(stack_is_not_empty)
 	{
 		CStack<std::string> stack;
 		stack.Push("Hello");
@@ -106,53 +134,53 @@ BOOST_AUTO_TEST_SUITE(Stack_have)
 
 	BOOST_AUTO_TEST_CASE(move_constructor)
 	{
-		CStack<std::string> moveingStack;
-		moveingStack.Push("string");
-		CStack<std::string> newStack(std::move(moveingStack));
+		CStack<std::string> movingStack;
+		movingStack.Push("string");
+		CStack<std::string> newStack(std::move(movingStack));
 
 		BOOST_CHECK_EQUAL(newStack.GetTop(), "string");
-		BOOST_REQUIRE_THROW(moveingStack.GetTop(), std::logic_error);
+		BOOST_REQUIRE_THROW(movingStack.GetTop(), std::logic_error);
 	}
 
 	BOOST_AUTO_TEST_CASE(copied_assign_operator)
 	{
-		CStack<int> pasteStack;
+		CStack<int> stackForInsertion;
 		CStack<int> copiedStack;
 
 		for (int i = -10; i < 10; i++)
 		{
-			pasteStack.Push(1);
+			stackForInsertion.Push(1);
 			copiedStack.Push(1);
 		}
 
-		pasteStack = copiedStack;
+		stackForInsertion = copiedStack;
 
-		while (!pasteStack.Empty() && !copiedStack.Empty())
+		while (!stackForInsertion.Empty() && !copiedStack.Empty())
 		{
-			BOOST_CHECK_EQUAL(pasteStack.GetTop(), copiedStack.GetTop());
-			pasteStack.Pop();
+			BOOST_CHECK_EQUAL(stackForInsertion.GetTop(), copiedStack.GetTop());
+			stackForInsertion.Pop();
 			copiedStack.Pop();
 		}
 	}
 
-	BOOST_AUTO_TEST_CASE(moveing_assign_operator)
+	BOOST_AUTO_TEST_CASE(moving_assign_operator)
 	{
-		CStack<int> pasteStack;
-		CStack<int> moveingStack;
+		CStack<int> stackForInsertion;
+		CStack<int> movingStack;
 
 		for (int i = -10; i < 10; i++)
 		{
-			moveingStack.Push(1);
+			movingStack.Push(1);
 		}
 
-		BOOST_CHECK_NO_THROW(moveingStack.GetTop());
-		pasteStack = std::move(moveingStack);
-		BOOST_REQUIRE_THROW(moveingStack.GetTop(), std::logic_error);
+		BOOST_CHECK_NO_THROW(movingStack.GetTop());
+		stackForInsertion = std::move(movingStack);
+		BOOST_REQUIRE_THROW(movingStack.GetTop(), std::logic_error);
 
-		while (!pasteStack.Empty())
+		while (!stackForInsertion.Empty())
 		{
-			BOOST_CHECK_EQUAL(pasteStack.GetTop(), 1);
-			pasteStack.Pop();
+			BOOST_CHECK_EQUAL(stackForInsertion.GetTop(), 1);
+			stackForInsertion.Pop();
 		}
 	}
 
@@ -164,6 +192,40 @@ BOOST_AUTO_TEST_SUITE(Stack_can_throw_exception_when_)
 	{
 		CStack<std::string> stack;
 		BOOST_REQUIRE_THROW(stack.GetTop(), std::logic_error);
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(Stack_copied_assign_operator)
+
+	BOOST_AUTO_TEST_CASE(can_work_with_itself)
+	{
+		const float number = 12.0099f;
+		CStack<float> simpleStack;
+		simpleStack.Push(number);
+
+		simpleStack = simpleStack;
+
+		BOOST_CHECK_EQUAL(simpleStack.GetTop(), number);
+		simpleStack.Pop();
+		BOOST_REQUIRE_THROW(simpleStack.GetTop(), std::logic_error);
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(Stack_moving_assign_operator)
+
+	BOOST_AUTO_TEST_CASE(can_work_with_itself)
+	{
+		const float number = 12.0099f;
+		CStack<float> simpleStack;
+		simpleStack.Push(number);
+
+		simpleStack = std::move(simpleStack);
+
+		BOOST_CHECK_EQUAL(simpleStack.GetTop(), number);
+		simpleStack.Pop();
+		BOOST_REQUIRE_THROW(simpleStack.GetTop(), std::logic_error);
 	}
 
 BOOST_AUTO_TEST_SUITE_END()
